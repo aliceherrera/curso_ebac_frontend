aeb63c175fe7bc5af5b261346d7e8106e76290ed
Alice Herrera <142321755+aliceherrera@users.noreply.github.com>
2024-05-13 16:37:26 -0300

    add files

---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function Multiply(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+console.log(Multiply(5, 5));
+
+function dizOi(nome: string): string {
+    return 'Olá ' + nome;
+}
+
+console.log(dizOi('Caio'));
